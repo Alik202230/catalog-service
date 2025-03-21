16cb7ffd514540135d8131b8f7d6c3daa98e3588
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2025-03-21 04:27:27 +0400

    Initial commit

---
+++ src/main/java/am/itspace/catalog/service/HelloController.java
@@ -0,0 +1,14 @@
+package am.itspace.catalog.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello World!";
+  }
+
+}
